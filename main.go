1ac063691ec41116f5e84bcd5a5645d8b177d35b
Leo Balduf <leobalduf@gmail.com>
2021-10-26 16:56:50 +0200

    *: make CI happy

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ipfs/go-ipfs/plugin"
 )
 
+// Plugins lists implementations of the `plugin.Plugin` interface exported by this IPFS plugin.
 var Plugins = []plugin.Plugin{
 	&pl.MetricExporterPlugin{},
 }
