2ea4543a95d8ab85d2834694686a6552ed7b9177
Leo Balduf <leobalduf@gmail.com>
2021-11-01 19:00:38 +0100

    *: add basic API, TCP logging, clean up

--- metricplugin/metric_export.go
+++ metricplugin/metric_export.go
@@ -1,11 +1,9 @@
 package metricplugin
 
 import (
-	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io"
-	"os"
 	"strings"
 	"time"
 
@@ -15,29 +13,70 @@ import (
 	bs "github.com/ipfs/go-bitswap"
 	core "github.com/ipfs/go-ipfs/core"
 	"github.com/ipfs/go-ipfs/plugin"
-	peer "github.com/libp2p/go-libp2p-core/peer"
+	logging "github.com/ipfs/go-log"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 const (
-	pollInterval = 10 * time.Second
+	// The default interval at which to populate prometheus with peer list
+	// metrics.
+	defaultQueryPeerListIntervalSeconds int = 10
 )
+
+var (
+	_ PluginAPI                   = (*MetricExporterPlugin)(nil)
+	_ plugin.PluginDaemonInternal = (*MetricExporterPlugin)(nil)
+
+	// Implementing io.Closer causes the IPFS daemon to gracefully shut down
+	// our plugin.
+	_ io.Closer = (*MetricExporterPlugin)(nil)
+)
+
+var log = logging.Logger("metric-export")
 
 // MetricExporterPlugin holds the state of the metrics exporter plugin.
 type MetricExporterPlugin struct {
-	api      *core.IpfsNode
-	bsEngine *bs.Bitswap
-	conf     Config
+	api  *core.IpfsNode
+	conf Config
+
+	wiretap *BitswapWireTap
+
+	// If not nil, a reference to the TCP server.
+	// Used for clean shutdown.
+	tcpServer *tcpServer
 }
 
-// Config contains all values configurated via the standard IPFS config section on plugins.
+// Subscribe implements PluginAPI.
+func (mep *MetricExporterPlugin) Subscribe(subscriber EventSubscriber) error {
+	// This is racy, but probably fine
+	if mep.wiretap != nil {
+		return mep.wiretap.subscribe(subscriber)
+	}
+	panic("Subscribe called but Bitswap Wiretap not set up")
+}
+
+// Unsubscribe implements PluginAPI.
+func (mep *MetricExporterPlugin) Unsubscribe(subscriber EventSubscriber) {
+	// This is racy, but probably fine
+	if mep.wiretap != nil {
+		mep.wiretap.unsubscribe(subscriber)
+	}
+}
+
+// Ping implements PluginAPI.
+func (*MetricExporterPlugin) Ping() {}
+
+// Config contains all values configured via the standard IPFS config section on plugins.
 type Config struct {
+	// The interval at which to populate prometheus with statistics about
+	// currently connected peers.
+	// Set to zero to deactivate.
 	QueryPeerListIntervalSeconds int `json:"QueryPeerListIntervalSeconds"`
+
+	// Configuration of the TCP server config.
+	// If this is nil, the TCP logging will not be activated.
+	TCPServerConfig *TCPServerConfig `json:"TCPServerConfig"`
 }
-
-var gwFilePath = "gateway_list.csv"
-
-var _ plugin.PluginDaemonInternal = (*MetricExporterPlugin)(nil)
 
 // Name returns the name of this plugin.
 // This is part of the `plugin.Plugin` interface.
@@ -55,15 +94,26 @@ func (*MetricExporterPlugin) Version() string {
 // This is part of the `plugin.Plugin` interface.
 func (mep *MetricExporterPlugin) Init(env *plugin.Environment) error {
 	// Populate the config
-	jconf, err := json.Marshal(env.Config)
+	jConf, err := json.Marshal(env.Config)
 	if err != nil {
 		return errors.Wrap(err, "unable to marshal plugin config to JSON")
 	}
 
 	var pConf Config
-	err = json.Unmarshal(jconf, &pConf)
+	err = json.Unmarshal(jConf, &pConf)
 	if err != nil {
 		return errors.Wrap(err, "unable to unmarshal plugin config from JSON")
+	}
+
+	if pConf.QueryPeerListIntervalSeconds < 0 {
+		log.Errorf("invalid QueryPeerListIntervalSeconds, using default (%d)", defaultQueryPeerListIntervalSeconds)
+		pConf.QueryPeerListIntervalSeconds = defaultQueryPeerListIntervalSeconds
+	} else if pConf.QueryPeerListIntervalSeconds == 0 {
+		log.Info("QueryPeerListIntervalSeconds is zero, these metrics will not be published to prometheus.")
+	}
+
+	if pConf.TCPServerConfig == nil {
+		log.Info("TCPServerConfig is nil, TCP logging disabled.")
 	}
 
 	mep.conf = pConf
@@ -74,7 +124,11 @@ func (mep *MetricExporterPlugin) Init(env *plugin.Environment) error {
 // Start starts this plugin.
 // This is not run in a separate goroutine and should not block.
 func (mep *MetricExporterPlugin) Start(ipfsInstance *core.IpfsNode) error {
-	fmt.Println("Metric Export Plugin started")
+	if !ipfsInstance.IsDaemon {
+		// Do not start in client mode.
+		// This should never happen.
+		panic("metric exporter plugin started with IPFS not in daemon mode")
+	}
 
 	// Register metrics
 	prometheus.MustRegister(trafficByGateway)
@@ -88,27 +142,45 @@ func (mep *MetricExporterPlugin) Start(ipfsInstance *core.IpfsNode) error {
 	if !ok {
 		return errors.New("could not get Bitswap implementation")
 	}
-	mep.bsEngine = bitswapEngine
 
-	// Read gateway list from file.
-	gwMap, err := readGatewayListFromFile(gwFilePath)
-	if err != nil {
-		return errors.Wrap(err, "unable to read list of gateways")
+	// Create a wiretap instance & subscribe to notifications in Bitswap &
+	// network.Network.
+	// We need to start this before we start the TCP server, so that clients can
+	// subscribe immediately without fail or races.
+	mep.wiretap = &BitswapWireTap{
+		api: ipfsInstance,
 	}
+	bs.EnableWireTap(mep.wiretap)(bitswapEngine)
 
-	// Create a wiretap instance & Subscribe to notifications in Bitswap & network.Network
-	bswt := &BitswapWireTap{
-		api:        ipfsInstance,
-		gatewayMap: gwMap,
-	}
-	bs.EnableWireTap(bswt)(bitswapEngine)
-
-	// TODO: For some reason this breaks everything and leads to a deadlock
-	// ipfsInstance.PeerHost.Network().Notify(bswt)
+	// Subscribe to network events.
+	ipfsInstance.PeerHost.Network().Notify(mep.wiretap)
 
 	// Periodically populate Prometheus with metrics about peer counts.
-	go mep.populatePromPeerCountLoop(time.Duration(mep.conf.QueryPeerListIntervalSeconds) * time.Second)
+	if mep.conf.QueryPeerListIntervalSeconds != 0 {
+		go mep.populatePromPeerCountLoop(time.Duration(mep.conf.QueryPeerListIntervalSeconds) * time.Second)
+	}
 
+	if mep.conf.TCPServerConfig != nil {
+		tcpServer, err := newTCPServer(mep, *mep.conf.TCPServerConfig)
+		if err != nil {
+			return errors.Wrap(err, "unable to start TCP server")
+		}
+		mep.tcpServer = tcpServer
+
+		go mep.tcpServer.listenAndServe()
+	}
+
+	fmt.Println("Metric Export Plugin started")
+	return nil
+}
+
+// Close implements io.Closer.
+// This is called to cleanly shut down the plugin when the daemon exits.
+func (mep *MetricExporterPlugin) Close() error {
+	// This is potentially racy, but probably not in our case.
+	if mep.tcpServer != nil {
+		mep.tcpServer.Shutdown()
+	}
 	return nil
 }
 
@@ -169,52 +241,4 @@ func (mep *MetricExporterPlugin) populatePromPeerCountLoop(interval time.Duratio
 			agentVersionCount.With(prometheus.Labels{"agent_version": av}).Inc()
 		}
 	}
-}
-
-func readGatewayListFromFile(path string) (map[peer.ID]string, error) {
-	gwFile, err := os.Open(path)
-	if err != nil {
-		return nil, errors.Wrapf(err, "unable to open gateway file %s", path)
-	}
-	defer gwFile.Close()
-
-	csvReader := csv.NewReader(gwFile)
-
-	// Skip the header.
-	_, err = csvReader.Read()
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to read CSV record")
-	}
-
-	// Read actual entries.
-	gatewayMap := make(map[peer.ID]string)
-	for {
-		line, err := csvReader.Read()
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return nil, errors.Wrap(err, "unable to read CSV record")
-		}
-		if len(line) != 2 {
-			return nil, fmt.Errorf("invalid number of fields in gateway CSV, expected 2, got %d", len(line))
-		}
-
-		// Decode the record:
-		// First field is the Peer ID, second field is the gateway operator.
-
-		peerID, err := peer.Decode(line[0])
-		if err != nil {
-			return nil, errors.Wrap(err, "unable to decode peer ID")
-		}
-		if _, ok := gatewayMap[peerID]; ok {
-			return nil, fmt.Errorf("duplicate peer ID %s in gateway CSV", peerID)
-		}
-
-		operator := strings.ToLower(line[1])
-
-		gatewayMap[peerID] = operator
-	}
-
-	return gatewayMap, nil
 }
